657af7e91d5818ecc0dc55619bdcd2b88e7d4424
zdpony <zd.pony@gmail.com>
2017-08-18 15:29:41 +0800

    lib

--- airlinerecovery_secondphase/src/sghku/tianchi/IntelligentAviation/HelloWorld2.java
+++ airlinerecovery_secondphase/src/sghku/tianchi/IntelligentAviation/HelloWorld2.java
@@ -3,5 +3,7 @@ package sghku.tianchi.IntelligentAviation;
 public class HelloWorld2 {
 	public static void main(String[] args){
 		System.out.println("hello world2!");
+		
+		System.out.println("changes made from pony's computer!");
 	}
 }
